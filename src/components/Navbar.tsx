b18987b4fe069cd073ebbefb5af6eacaa42f0f1e
Usman Nasir <usmannasir98@gmail.com>
2024-11-13 23:30:42 +0500

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='container pt-8'>
+        <div className='flex justify-between items-center'>
+            <div className='text-xl font-medium'>
+                Usman Nasir
+            </div>
+        </div>
+    </div>
+  )
+}
+export default Navbar
